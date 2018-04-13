7e1f9003302a192dc7a81e846dcf11fe8b6f27a8
root <chrbjorl@student.matnat.uio.no>
2018-04-13 09:54:31 +0200

    endret navn pa monodomenesolver

--- solver_monodomene.py
+++ solver_monodomene.py
@@ -1,7 +1,7 @@
 from fenics import *
 import numpy as np
 import time
-import scipy.sparse
+import matplotlib.pyplot as plt
 
 sigma = 0.03
 c_1 = 200
@@ -10,19 +10,19 @@ c_2 = 200
 c_3 = 1
 b = 1
 
-
-Nx = 500
+i = 4
+Nx = 32#4*2**i
 Ny = Nx
 
-degree = 1
-two_d = 0
-T = 0.5
-num_steps = 80000
+degree = 2
+two_d = 1
+T = 0.2
+num_steps = 2000#94*2**i
 
 system = True            # fitzhugh-nagumo if system = True
 heat = False
 plotting = False
-advance_method = "FE_system"           #"FE" if ForwardEuler and "OS" if OperatorSplitting
+advance_method = "FE"           #"FE" if ForwardEuler and "OS" if OperatorSplitting
 dt = T/float(num_steps)
 
 class ODEsolver:
@@ -43,9 +43,9 @@ class ODEsolver:
 
     def solver(self):
         ref = self.ref
-        filename_referanse = "%sd_%s_%s_%s_%s_%1.3f.dat" % (2**two_d, self.num_elements, \
+        filename = "%sd_%s_%s_%s_%s_%1.3f" % (2**two_d, self.num_elements, \
                                 self.num_steps, self.heat, self.advance_method, sigma)
-        outfile = open(filename_referanse, "w")
+        outfile = open(filename, "w")
         # time stepping
         for n in range(0, self.num_steps ):
             if system:
@@ -56,29 +56,27 @@ class ODEsolver:
             self.v_1_vector = v.vector()
             self.tid += self.dt
             self.exact_expression.t += self.dt
-            if n%400 == 0:
+            if n%100 == 0:
                   print self.tid
             # break if numerical solution diverges
-            if abs(np.sum(self.v_1_vector.array())/len(self.v_1_vector.array())) > 2:
+            if abs(np.sum(self.v_1_vector.get_local())/len(self.v_1_vector.get_local())) > 2:
                 print "break"
                 break
-            if self.plotting and n%2 == 0:
+            if self.plotting and n%int(num_steps/4) == 0:
                 plot(v)
+                plt.show()
         u_e = project(self.exact_expression, V)
         #write numerical solution to file
-        outfile.write("numerical solution at t=%1.8f, sigma = %1.8f" % (self.tid, sigma))
-        outfile.write("\n")
-        s = "%14.8f" % v.vector().array()[0]
-        for j in range(1,len(v.vector().array())):
-            s += "%14.8f " % v.vector().array()[j]
-        outfile.write(s + "\n")
+        output_file = XDMFFile(filename + ".xdmf")
+        output_file.write_checkpoint(v,'v')
+        File(filename + '_mesh.xml.gz') << v.function_space().mesh()
         if self.heat:
             # write exact solution to file
             outfile.write("exact solution at t=%1.8f" % self.tid)
             outfile.write("\n")
-            s = "%14.8f" % u_e.vector().array()[0]
-            for j in range(1,len(u_e.vector().array())):
-                s += "%14.8f" % u_e.vector().array()[j]
+            s = "%14.8f" % u_e.vector().get_local()[0]
+            for j in range(1,len(u_e.vector().get_local())):
+                s += "%14.8f" % u_e.vector().get_local()[j]
             outfile.write(s + "\n")
             outfile.close()
 
@@ -177,7 +175,7 @@ LHS = M - A*dt
 # lumped mass matrix
 # diagonal elements of M
 M.get_diagonal(y.vector())
-diag =  y.vector().array()
+diag =  y.vector().get_local()
 #create identity matrix
 I = M.copy()
 I.zero()
@@ -186,7 +184,7 @@ I.get_diagonal(y.vector())
 # diag2 contains the row sums of M
 M = assemble(m)
 S = M*y.vector()
-diag2 =  S.array()
+diag2 =  S.get_local()
 c = sum(diag2)/sum(diag)
 diag *= c
 diag = diag**(-1)
@@ -196,7 +194,7 @@ M_inv.zero()
 M_inv.set_diagonal(v.vector())
 # henter ut diagonalen til lumped mass matrix
 M_inv.get_diagonal(z.vector())
-print z.vector().array()
+print z.vector().get_local()
 M_inv_diag_vector = z.vector()
 
 object_fe = ForwardEuler(T = T, num_steps = num_steps, plotting = plotting, ref = True,\
@@ -220,4 +218,4 @@ elif advance_method == "FE_system":
 
 end = time.time()
 print end - start
-interactive()
+#interactive()
