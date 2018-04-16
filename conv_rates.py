23505f2386ebdcd13eec48a791867f5c4947ecc2
root <chrbjorl@student.matnat.uio.no>
2018-04-16 19:37:57 +0200

    endret solver_monodomene slik at man kan  importere solver fra denne modulen. conv_rates importerer na solver fra solver_monodomene

--- conv_rates.py
+++ conv_rates.py
@@ -1,29 +1,33 @@
 from fenics import *
 import numpy as np
 import time
+from solver_monodomene import *
 from math import log
 
 num_experiments = 1
 
-sigma = 1
-T = 1
+sigma = 0.03
+T = 0.2
 
-model = "bi"
+model = "mono"
+plotting = False
+num_steps_ref = 10000
+num_steps = [400 for i in range(11)]
+#num_steps = [50*2**i for i in range(8)]
 
-num_steps_ref = 200
-num_steps = [50, 100]
-
-num_elements_ref = 40
-num_elements =[10, 20]
-method_ref = "OS"
-method = "OS"
+num_elements_ref = 500
+num_elements =[40 + i for i in range(11)]
+#num_elements =[5*2**i for i in range(8)]
+method_ref = "FE"
+method = "FE"
 
 num_elements.append(num_elements_ref)
 num_steps.append(num_steps_ref)
 
 
 heat = False
-two_d = 1
+system = False
+two_d = 0
 degree = 1
 degree_ref = 1
 
@@ -39,27 +43,55 @@ def compute_rates(dt_values, E_values):
 
 vectors = []
 for j in range(len(num_steps)):
+    if j == len(num_steps) - 1:
+        method = method_ref
     if model == "bi":
-        filename = "bi_%sd_%s_%s_%s" % (2**two_d, num_elements[j], num_steps[j],
-                                        method)
-    else:
-        filename = "%sd_%s_%s_%s_%s_%1.3f" % (2**two_d, num_elements[j], num_steps[j],
-                                        heat, method, sigma)
+        filename = "bi_%sd_%s_%s_%s_%s" % (2**two_d, num_elements[j], num_steps[j],
+                                        method, num_jacobi_iter[j])
+        if method == "FE":
+            object_fe = ForwardEuler(T = T, num_steps = num_steps[j], plotting = plotting,\
+                                        num_elements = num_elements[j], system = system, heat = heat,
+                                        advance_method = advance_method)
+            solution_fe = object_fe.solver()
+
+        else:
+            object_os = OperatorSplitting(T = T, num_steps = num_steps[j], plotting = plotting,\
+                                        num_elements = num_elements[j], advance_method = advance_method)
+            solution_os = object_os.solver()
+
+    if model == "mono":
+        filename = "%sd_%s_%s_%s_%s_%s_%1.3f" % (2**two_d, num_elements[j], num_steps[j],
+                                                        heat, system, method, sigma)
+        if method == "FE":
+            object_fe = ForwardEuler(T = T, num_steps = num_steps[j], plotting = plotting,
+                                        num_elements = num_elements[j], system = system, heat = heat,
+                                        advance_method = advance_method, two_d = two_d, sigma = sigma)
+            solution_fe = object_fe.solver()
+        else:
+            object_fe = ForwardEuler(T = T, num_steps = num_steps[j], plotting = plotting,
+                                        num_elements = num_elements[j], system = system, heat = heat,
+                                        advance_method = advance_method, two_d = two_d, sigma = sigma)
+            solution_os = object_os.solver()
     input_file = XDMFFile(filename+'.xdmf')
+    print filename
     mesh = Mesh(filename+'_mesh.xml.gz')
     V_ref = FunctionSpace(mesh, 'P', degree)
     u = Function(V_ref)
     input_file.read_checkpoint(u,'v')
     vectors.append(u)
 
+v_ref = vectors[-1]
 for j in range(0, len(vectors) - 1, 1):
-    v_ref = vectors[-1]
-    v = interpolate(vectors[j], v_ref.ufl_function_space())
-    error_L2 = errornorm(v, v_ref, "L2")
+    v = vectors[j]
+    v_ref_interpolate = interpolate(v_ref, v.ufl_function_space())
+    #print v_ref_interpolate.vector().get_local()
+    error_L2 = errornorm(v_ref_interpolate, v, "L2")
     Error_values.append(error_L2)
-    array = v.vector().get_local()
-    print error_L2
-
 
 print Error_values
-print compute_rates(dt_values, Error_values)
+
+conv_rates = compute_rates(dt_values, Error_values)
+print '    dt       dx    L2 error  conv. rate'
+for j in range(len(conv_rates)):
+    print "%6.6f%7.8f%10.6f%12.4f" % (dt_values[j+1], num_elements[j+1],
+                                Error_values[j+1],conv_rates[j])
--- solver_monodomene.py
+++ solver_monodomene.py
@@ -4,47 +4,125 @@ import time
 import matplotlib.pyplot as plt
 
 sigma = 0.03
-c_1 = 200
-a_1 = 0.1
-c_2 = 200
-c_3 = 1
-b = 1
+#c_1 = 200
+#a_1 = 0.1
+#c_2 = 200
+#c_3 = 1
+#b = 1
 
-i = 4
-Nx = 32#4*2**i
+Nx = 40
 Ny = Nx
 
-degree = 2
-two_d = 1
+degree = 1
+two_d = 0
 T = 0.2
-num_steps = 2000#94*2**i
+num_steps = 1000
 
-system = True            # fitzhugh-nagumo if system = True
+system = False         # fitzhugh-nagumo if system = True
 heat = False
-plotting = False
+plotting = True
 advance_method = "FE"           #"FE" if ForwardEuler and "OS" if OperatorSplitting
 dt = T/float(num_steps)
 
 class ODEsolver:
-    def __init__(self, T, num_steps, plotting, ref, num_elements,
-                system, heat, exact_expression, advance_method):
+    def __init__(self, T, num_steps, plotting, num_elements,
+                system, heat, advance_method, two_d, sigma,
+                c_1 = 200, a_1 = 0.1, c_2 = 200, c_3 = 1, b = 1):
         self.T, self.num_steps = T, num_steps
-        self.plotting, self.ref = plotting, ref
-        self.v_1_vector = v_0.vector()
-        self.w_1_vector = w_0.vector()
+        self.plotting = plotting
         self.dt = T/float(num_steps)
         self.num_elements = num_elements
         self.tid = 0
         self.system = system
         self.heat = heat
-        self.exact_expression = exact_expression
         self.advance_method = advance_method
+        self.two_d = two_d
+        self.c_1, self.a_1, self.c_2, self.c_3, self.b = c_1, a_1, c_2, c_3, b
+        Nx = num_elements
+        Ny = Nx
+
+        # create Expressions
+        if two_d:
+            if heat:
+                I_v_expression = Expression("sigma*cos(pi*x[0])", degree = 2, sigma = sigma)
+            else:
+                I_v_expression = Expression("pow(x[0], 2) + pow(x[1], 2) < 0.2 ? 1: 0", degree = 2)
+            I_w_expression = Expression("x[0] < 0.2 && x[1] < 0.2 ? 0: 0", degree = 1)
+            mesh = UnitSquareMesh(Nx, Ny)
+
+        else:
+            if heat:
+                I_v_expression = Expression("sigma*cos(pi*x[0])", degree = 2, sigma = sigma)
+            else:
+                I_v_expression = Expression("x[0] < 0.2 ? 1: 0", degree = 2)
+            I_w_expression = Expression("x[0] < 0.2 ? 0: 0", degree = 2)
+            mesh = UnitIntervalMesh(Nx)
+
+        exact_expression = Expression("sigma*exp(-pi*pi*t)*cos(pi*x[0])", degree = 2, t = 0,
+                                        sigma = sigma)
+
+        #create function space
+        V = FunctionSpace(mesh, "P", degree)
+
+        #interpolate initial condition
+        v_0 = interpolate(I_v_expression, V)
+        w_0 = interpolate(I_w_expression, V)
+
+        # Define variational problem
+        v = TrialFunction(V)
+        psi = TestFunction(V)
+        v_n_s2 = Function(V)
+        a = dot(-sigma*grad(v), grad(psi))*dx
+        m = dot(v, psi)*dx
+
+        # assemble A outside time loop, since A is time-independent
+        A = assemble(a)
+        M = assemble(m)
+        v = Function(V)
+        w = Function(V)
+        y = Function(V)
+        z = Function(V)
+        LHS = M - A*dt
+
+        # lumped mass matrix
+        # diagonal elements of M
+        M.get_diagonal(y.vector())
+        diag =  y.vector().get_local()
+        #create identity matrix
+        I = M.copy()
+        I.zero()
+        I.set_diagonal(interpolate(Constant(1), V).vector())
+        I.get_diagonal(y.vector())
+        # diag2 contains the row sums of M
+        M = assemble(m)
+        S = M*y.vector()
+        diag2 =  S.get_local()
+        c = sum(diag2)/sum(diag)
+        diag *= c
+        diag = diag**(-1)
+        v.vector()[:] = diag[:]
+        M_inv = I
+        M_inv.zero()
+        M_inv.set_diagonal(v.vector())
+        # henter ut diagonalen til lumped mass matrix
+        M_inv.get_diagonal(z.vector())
+        M_inv_diag_vector = z.vector()
+        self.v_1_vector = v_0.vector()
+        self.w_1_vector = w_0.vector()
+        self.M_inv = M_inv
+        self.M = M
+        self.A = A
+        self.LHS = LHS
+        self.sigma = sigma
+        self.v, self.w, self.V = v, w, V
+        self.exact_expression = exact_expression
+
 
 
     def solver(self):
-        ref = self.ref
-        filename = "%sd_%s_%s_%s_%s_%1.3f" % (2**two_d, self.num_elements, \
-                                self.num_steps, self.heat, self.advance_method, sigma)
+        v, w = self.v, self.w
+        filename = "%sd_%s_%s_%s_%s_%s_%1.3f" % (2**self.two_d, self.num_elements, \
+                                self.num_steps, self.heat, self.system, self.advance_method, self.sigma)
         outfile = open(filename, "w")
         # time stepping
         for n in range(0, self.num_steps ):
@@ -62,10 +140,10 @@ class ODEsolver:
             if abs(np.sum(self.v_1_vector.get_local())/len(self.v_1_vector.get_local())) > 2:
                 print "break"
                 break
-            if self.plotting and n%int(num_steps/4) == 0:
+            if self.plotting and n%int(self.num_steps/2) == 0:
                 plot(v)
                 plt.show()
-        u_e = project(self.exact_expression, V)
+        u_e = project(self.exact_expression, self.V)
         #write numerical solution to file
         output_file = XDMFFile(filename + ".xdmf")
         output_file.write_checkpoint(v,'v')
@@ -88,9 +166,12 @@ class OperatorSplitting(ODEsolver):
         v_1_vector = self.v_1_vector
         w_1_vector = self.w_1_vector
         dt = self.dt
+        LHS = self.LHS
+        M = self.M
         # step 1
         RHS = M*v_1_vector
         solve(LHS, v_n_s2.vector(), RHS, "cg")
+        c_1, a_1, c_2, c_3, b = self.c_1, self.a_1, self.c_2, self.c_3, self.b
         if self.system:
             # step 2
             v_n_s3 = v_n_s2.vector() + dt*(c_1*v_n_s2.vector()*\
@@ -107,14 +188,24 @@ class ForwardEuler(ODEsolver):
             w_1_vector = self.w_1_vector
         v_1_vector = self.v_1_vector
         dt = self.dt
+        dt = self.dt
+        LHS = self.LHS
+        M = self.M
+        M_inv = self.M_inv
+        A = self.A
+        product1 = dt*A*v_1_vector
+        c_1, a_1, c_2, c_3, b = self.c_1, self.a_1, self.c_2, self.c_3, self.b
         if self.system:
-            product1 = dt*A*v_1_vector
             return M_inv*product1 + v_1_vector + dt*c_1*v_1_vector*\
             (v_1_vector - a_1)*(1 - v_1_vector) - dt*c_2*v_1_vector*w_1_vector, \
             dt*b*(v_1_vector - c_3*w_1_vector) + w_1_vector
         if self.heat:
             product1 = dt*A*v_1_vector
             return M_inv*product1 + v_1_vector
+        if not self.heat and not self.system:
+            return M_inv*product1 + v_1_vector + dt*c_1*v_1_vector*\
+                    (v_1_vector - a_1)*(1 - v_1_vector)
+
 
 class ForwardEulerSystem(ODEsolver):
     def advance(self):
@@ -127,95 +218,28 @@ class ForwardEulerSystem(ODEsolver):
                 dt*b*(v_1_vector - c_3*w_1_vector) + w_1_vector
 
 
-start = time.time()
-
-# create Expressions
-if two_d:
-    if heat:
-        I_v_expression = Expression("sigma*cos(pi*x[0])", degree = 2, sigma = sigma)
-    else:
-        I_v_expression = Expression("pow(x[0], 2) + pow(x[1], 2) < 0.2 ? 1: 0", degree = 2)
-    I_w_expression = Expression("x[0] < 0.2 && x[1] < 0.2 ? 0: 0", degree = 1)
-    mesh = UnitSquareMesh(Nx, Ny)
-
-else:
-    if heat:
-        I_v_expression = Expression("sigma*cos(pi*x[0])", degree = 2, sigma = sigma)
-    else:
-        I_v_expression = Expression("x[0] < 0.2 ? 1: 0", degree = 2)
-    I_w_expression = Expression("x[0] < 0.2 ? 0: 0", degree = 2)
-    mesh = UnitIntervalMesh(Nx)
-
-exact_expression = Expression("sigma*exp(-pi*pi*t)*cos(pi*x[0])", degree = 2, t = 0,
-                                sigma = sigma)
-
-#create function space
-V = FunctionSpace(mesh, "P", degree)
-
-#interpolate initial condition
-v_0 = interpolate(I_v_expression, V)
-w_0 = interpolate(I_w_expression, V)
-
-# Define variational problem
-v = TrialFunction(V)
-psi = TestFunction(V)
-v_n_s2 = Function(V)
-a = dot(-sigma*grad(v), grad(psi))*dx
-m = dot(v, psi)*dx
-
-# assemble A outside time loop, since A is time-independent
-A = assemble(a)
-M = assemble(m)
-v = Function(V)
-w = Function(V)
-y = Function(V)
-z = Function(V)
-LHS = M - A*dt
-
-# lumped mass matrix
-# diagonal elements of M
-M.get_diagonal(y.vector())
-diag =  y.vector().get_local()
-#create identity matrix
-I = M.copy()
-I.zero()
-I.set_diagonal(interpolate(Constant(1), V).vector())
-I.get_diagonal(y.vector())
-# diag2 contains the row sums of M
-M = assemble(m)
-S = M*y.vector()
-diag2 =  S.get_local()
-c = sum(diag2)/sum(diag)
-diag *= c
-diag = diag**(-1)
-v.vector()[:] = diag[:]
-M_inv = I
-M_inv.zero()
-M_inv.set_diagonal(v.vector())
-# henter ut diagonalen til lumped mass matrix
-M_inv.get_diagonal(z.vector())
-print z.vector().get_local()
-M_inv_diag_vector = z.vector()
-
-object_fe = ForwardEuler(T = T, num_steps = num_steps, plotting = plotting, ref = True,\
+if __name__ == "__main__":
+    start = time.time()
+    object_fe = ForwardEuler(T = T, num_steps = num_steps, plotting = plotting, \
                             num_elements = Nx, system = system, heat = heat,
-                             exact_expression = exact_expression, advance_method = advance_method)
+                              advance_method = advance_method, two_d = two_d,
+                              sigma = sigma)
 
-object_os = OperatorSplitting(T = T, num_steps = num_steps, plotting = plotting, ref = True,\
+    object_os = OperatorSplitting(T = T, num_steps = num_steps, plotting = plotting,
                                     num_elements = Nx, system = system, heat = heat,
-                                    exact_expression = exact_expression, advance_method = advance_method)
-object_fe_system = ForwardEulerSystem(T = T, num_steps = num_steps, plotting = plotting, ref = True,\
+                                     advance_method = advance_method, two_d = two_d,
+                                     sigma = sigma)
+    object_fe_system = ForwardEulerSystem(T = T, num_steps = num_steps, plotting = plotting,
                                     num_elements = Nx, system = system, heat = heat,
-                                    exact_expression = exact_expression, advance_method = advance_method)
+                                    advance_method = advance_method, two_d = two_d,
+                                    sigma = sigma)
+    if advance_method == "FE":
+        solution_fe = object_fe.solver()
+    elif advance_method == "OS":
+        solution_os = object_os.solver()
+    elif advance_method == "FE_system":
+        solution_os = object_fe_system.solver()
 
-
-if advance_method == "FE":
-    solution_fe = object_fe.solver()
-elif advance_method == "OS":
-    solution_os = object_os.solver()
-elif advance_method == "FE_system":
-    solution_os = object_fe_system.solver()
-
-end = time.time()
-print end - start
-#interactive()
+    end = time.time()
+    print end - start
+    #interactive()
