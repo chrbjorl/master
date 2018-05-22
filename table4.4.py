86c4928fa8e579bd0cdd3f9d95df031a37e23708
root <chrbjorl@student.matnat.uio.no>
2018-05-22 16:04:06 +0200

    added file which reproduces table

---
+++ table4.4.py
@@ -0,0 +1,11 @@
+from conv_rates_monodomene import *
+from efficiency import error_
+
+two_d = 0
+mono = True
+advance_method = "OS"
+num_steps = [40*2**i for i in range(4)]
+num_elements = [520*2**i for i in range(4)]
+
+table(num_steps = num_steps, num_elements = num_elements,
+    advance_method = advance_method, two_d = False, mono = mono)
