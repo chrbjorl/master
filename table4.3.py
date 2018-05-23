d3f66f65341feb038238cb5a745e21101b08ddd3
root <chrbjorl@student.matnat.uio.no>
2018-05-23 13:24:06 +0200

    small changes

--- table4.3.py
+++ table4.3.py
@@ -1,10 +1,10 @@
-from conv_rates_monodomene import *
+from conv_rates import *
 
-two_d = 0
+two_d = False
 mono = True
 advance_method = "FE"
 num_steps = [520*2**i for i in range(4)]
 num_elements = [40*2**i for i in range(4)]
 
-table(num_steps = num_steps, num_elements = num_elements, advance_method = "FE",
-    two_d = False, mono = mono)
+table(num_steps = num_steps, num_elements = num_elements, advance_method = advance_method,
+    two_d = two_d, mono = mono)
